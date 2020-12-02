e1c1a9cce2a05b3e9c8defaa86aa8821dfa875db
Yizi Chen <ychen@node9.lrde.epita.fr>
2020-12-02 11:56:34 +0100

    add new function

---
+++ code/DGMM_release_code/1.Prepare_tilling/boarder_calibration.py
@@ -0,0 +1,27 @@
+import argparse
+import cv2
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Boarder Calibration.')
+    parser.add_argument('input_EPM_path', help='Path of edge probaiblity map.')
+    parser.add_argument('input_boarder_path', help='Path of boarder mask.')
+    parser.add_argument('output_path', help='Path of update boader image.')
+
+    args = parser.parse_args()
+
+    EPM = cv2.imread(args.input_EPM_path, cv2.IMREAD_GRAYSCALE)
+    BOD = cv2.imread(args.input_boarder_path, cv2.IMREAD_GRAYSCALE)
+
+    # for each pixel p(x,y) where value = 0 in the mask, you should set the
+    # EPM pixel p(x,y) to 0 (sure no edge)
+    # for p(x,y) = 255 in mask: set p(x,y) = 255 in EPM (sure edge)
+    # for p(x,y) = 128 in mask: keep p(x,y) original value in EPM.
+    EPM[BOD == 0] = 0
+    EPM[BOD == 255] = 255
+
+    cv2.imwrite(args.output_path, EPM)
+    print('Save calibration results to {}'.format(args.output_path))
+
+if __name__ == '__main__':
+    main()
---
+++ code/DGMM_release_code/1.Prepare_tilling/create_tilling.py
@@ -0,0 +1,43 @@
+import skimage
+import skimage.io as skio
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from skimage.util import view_as_windows
+import argparse
+
+def generate_tilling(image_path, save_path, w_size=500):
+    # Generate tilling images
+    win_size = w_size
+    pad_px = win_size // 2
+
+    # Read image
+    in_img = skio.imread(image_path)
+
+    # Pad image
+    img_pad = np.pad(in_img, [(pad_px,pad_px), (pad_px,pad_px), (0,0)], 'edge')
+
+    # View as window
+    tiles = view_as_windows(img_pad, (win_size,win_size,3), step=pad_px)
+
+    for row in range(tiles.shape[0]):
+        for col in range(tiles.shape[1]):
+            tt = tiles[row, col, 0, ...].copy()
+            # If you want black boarder, set the value to 255 (Suggest not using balck boarder)
+            # bordersize=100
+            # tt[:bordersize,:, 2] = 255
+            # tt[-bordersize:,:, 2] = 255
+            # tt[:,:bordersize, 2] = 255
+            # tt[:,-bordersize:, 2] = 255
+            skio.imsave(os.path.join(save_path, f"t_r{row:02d}_c{col:02d}.jpg"), tt)
+
+def main():
+    parser = argparse.ArgumentParser(description='Boarder Calibration.')
+    parser.add_argument('input_path', help='Path of original image.')
+    parser.add_argument('output_path', help='Directory of saving the tillings.')
+
+    args = parser.parse_args()
+    generate_tilling(args.input_path, args.output_path)
+
+if __name__ == '__main__':
+    main()
